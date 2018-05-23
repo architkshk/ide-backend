9a5ed903712dd32ce001b2a6ffef3b99dac262f1
Archit Kaushik <asarchit65@gmail.com>
2018-05-23 17:38:21 +0530

    updated

--- util/userdetails.js
+++
@@ -1,8 +0,0 @@
-const db = require('./db');
-
-function getUserDetails(userId){
-    const details = db.User.findById(userId);
-    return Promise.all([user]);
-};
-
-exports = module.exports = getUserDetails;
